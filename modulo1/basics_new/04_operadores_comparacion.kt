b75eb2e348260593cd3661fc2a40feafb4c8dc3f
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-04-07 19:58:02 -0500

    Clase 07-04-2026

---
+++ modulo1/basics_new/04_operadores_comparacion.kt
@@ -0,0 +1,22 @@
+// Operadores de Comparacion
+fun main() {
+    var numero1= 10 
+    var numero2= 2
+    
+    
+    println("numero1==numero2= ${numero1==numero2}")
+    println("numero1!=numero2= ${numero1!=numero2}")
+    println("numero1>numero2= ${numero1>numero2}")
+    println("numero1<numero2= ${numero1<numero2}")
+    println("numero1>=numero2= ${numero1>=numero2}")
+    println("numero1<=numero2= ${numero1<=numero2}")
+    
+    var string1 = "kotlin"
+    var string2 = "kotlin"
+    
+    println("string1===string2 ${string1===string2}")
+    println("string1==string2 ${string1==string2}")
+    println("string1.equals(string2) ${string1.equals(string2)}")
+    
+
+}
