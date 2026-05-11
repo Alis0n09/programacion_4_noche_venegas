03506c8ff1d3479bebb08725689afd8b72bac192
Alis0n09 <aliliseth2006@hotmail.com>
2026-05-11 15:06:35 -0500

    Clase del 05/05/2026 y 07/05/2026

--- modulo2/tts/app/src/main/java/com/ute/tts/MainActivity.kt
+++ modulo2/tts/app/src/main/java/com/ute/tts/MainActivity.kt
@@ -1,128 +1,44 @@
 package com.ute.tts
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.webkit.WebChromeClient
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import android.widget.Button
-import android.widget.MediaController
-import android.widget.VideoView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 
 class MainActivity : AppCompatActivity() {
 
-
-    private lateinit var webView: WebView
-    private lateinit var videoView: VideoView
-
-
-    private val youtubeWatchUrl = "https://www.youtube.com/watch?v=6BODDyZRF6A"
-    private val youtubeEmbedUrl="https://www.youtube.com/embed/kXYiU_JCYtU?playsinline=1&rel=0"
-
+    private var contador = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_multimedia)
+        setContentView(R.layout.activity_main_contador)
 
+        val textView = findViewById<TextView>(R.id.textViewContador)
+        val btnSumar = findViewById<Button>(R.id.btnSumar)
+        val btnRestar = findViewById<Button>(R.id.btnRestar)
+        val btnReset = findViewById<Button>(R.id.btnReset)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(16, systemBars.top + 16, 16, systemBars.bottom + 16)
-            insets
+        fun actualizarTexto() {
+            textView.text = contador.toString()
         }
 
-
-        webView = findViewById(R.id.webViewYouTube)
-
-
-        val ws: WebSettings = webView.settings
-        ws.javaScriptEnabled = true
-        ws.domStorageEnabled = true
-        ws.mediaPlaybackRequiresUserGesture = false
-        ws.loadWithOverviewMode = true
-        ws.useWideViewPort = true
-
-
-        webView.webChromeClient = WebChromeClient()
-        webView.webViewClient = WebViewClient()
-
-
-        val html = """
-           <!DOCTYPE html>
-           <html>
-           <head>
-               <meta name="viewport" content="width=device-width, initial-scale=1.0">
-               <style>
-                   html, body {
-                       margin: 0;
-                       height: 100%;
-                       background: #000;
-                   }
-                   iframe {
-                       width: 100%;
-                       height: 100%;
-                       border: 0;
-                   }
-               </style>
-           </head>
-           <body>
-               <iframe
-                   src="$youtubeEmbedUrl"
-                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                   allowfullscreen>
-               </iframe>
-           </body>
-           </html>
-       """.trimIndent()
-
-
-        webView.loadDataWithBaseURL(
-            "https://www.youtube.com",
-            html,
-            "text/html",
-            "UTF-8",
-            null
-        )
-
-
-        findViewById<Button>(R.id.btnOpenYouTube).setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeWatchUrl))
-            startActivity(intent)
+        btnSumar.setOnClickListener {
+            contador++
+            actualizarTexto()
         }
 
-
-        videoView = findViewById(R.id.videoViewMp4)
-
-
-        val mediaController = MediaController(this)
-        mediaController.setAnchorView(videoView)
-        videoView.setMediaController(mediaController)
-
-
-        val videoUri: Uri = Uri.parse("android.resource://$packageName/${R.raw.demo}")
-        videoView.setVideoURI(videoUri)
-
-
-        videoView.setOnPreparedListener { mp ->
-            mp.isLooping = false
-            videoView.start()
+        btnRestar.setOnClickListener {
+            if (contador > 0) contador--
+            actualizarTexto()
         }
+
+        btnReset.setOnClickListener {
+            contador = 0
+            actualizarTexto()
+        }
+
+        actualizarTexto()
     }
-
-
-   /* override fun onBackPressed() {
-        if (this::webView.isInitialized && webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
-    }*/
 }
